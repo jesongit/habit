6ffb6fd61838b3a92595174993ed14b3eeaa6920
jesongit <188380780@qq.com>
2023-02-13 22:48:30 +0800

    init

---
+++ src/test/java/com/example/habit/HabitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.habit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
